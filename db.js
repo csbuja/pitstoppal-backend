433d9d85dab8bd3acd52ed579efa820e8dd686c5
Christopher Buja <csbuja@umich.edu>
2016-08-29 21:27:34 -0700

    got db js heroku ready

--- db.js
+++ db.js
@@ -1,25 +1,35 @@
 var mysql = require("mysql");
+var _ = require("underscore")
 
 //spencer - /tmp/mysql.sock', // use mysqladmin variables | grep sock to get the socket path
 // server  '/var/run/mysqld/mysqld.sock', // use mysqladmin variables | grep sock to get the socket path
-	// First you need to create a connection to the db
-var con = mysql.createConnection({
-	host     : 'localhost',
-	user     : 'root',
-	socketPath  : '/tmp/mysql.sock', // use mysqladmin variables | grep sock to get the socket path
-	database: 'eecs498',
+	// First you need to create a connection to thes db
+var db_url = ""
+if (_.isUndefined(process.env.CLEARDB_DATABASE_URL) ){// not on heroku server {
+	db_url = "localhost";
+}
+else { //on heroku server
+	db_url=process.env.CLEARDB_DATABASE_URL;
+} 
 
-});
+	var con = mysql.createConnection({
+		host     : db_url,
+		user     : 'root',
+		socketPath  : '/tmp/mysql.sock', // use mysqladmin variables | grep sock to get the socket path
+		database: 'eecs498',
+
+	});
 
 
-con.connect(function(err){
-	if(err){
-				console.log('Error connecting to Db');
-				console.log(err)
-				return;
-	};
-		
-	console.log('Connection established');
-});
+	con.connect(function(err){
+		if(err){
+					console.log('Error connecting to Db');
+					console.log(err)
+					return;
+		};
+			
+		console.log('Connection established');
+	});
+
 
 module.exports = con;
